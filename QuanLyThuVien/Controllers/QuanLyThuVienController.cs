1763c2ee7daf516e5081ae2e39b55540c1ae8d83
Akkay <nguyenhung.dev.info@gmail.com>
2023-10-17 20:31:47 +0700

    - Trang chủ - Trang Login

---
+++ QuanLyThuVien/Controllers/QuanLyThuVienController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace QuanLyThuVien.Controllers
+{
+    public class QuanLyThuVienController : Controller
+    {
+        // GET: QuanLyThuVIen
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
